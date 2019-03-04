14297ba702882175b15c66f87eb8930af28f4c65
JP Clark <jc7553a@student.american.edu>
2019-03-04 15:37:07 -0500

    Updated Python Code

--- autoencoder_scrach.py
+++ autoencoder_scrach.py
@@ -1,7 +1,7 @@
 import numpy as np
 import random as ra
 import matplotlib.pyplot as plt
-from sklearn.preprocessing import normalize
+from sklearn.preprocessing import MinMaxScaler
 
 learning_rate= .01
 input_size = 3
@@ -21,7 +21,7 @@ def backpropogate(w1, w2, vector, out,hidden, b1, b2):
     adj_bias2 = 0.0
     for i in range(input_size):
         delta[i][0] = (out[i][0] - vector[0][i])*out[i][0]*(1-out[i][0])
-        adj_bias2 -= learning_rate*delta[i][0]*b1
+        adj_bias2 -= learning_rate*delta[i][0]*b2
     adj_matrix2 = np.matmul(delta, hidden.transpose())
     adj_matrix2 = adj_matrix2 +(-1*learning_rate)
     hidden2 = (hidden*-1)+1
@@ -47,16 +47,32 @@ def feedforward(w1, w2, b1, b2, vector):
     out = sigmoid(out)
     return out, hidden
 
-
+def generate_normal():
+    a = np.zeros((100, 3))
+    for i in range(100):
+        a[i][0] = ra.gauss(2, .01)
+        a[i][1] = ra.gauss(13, .01)
+        a[i][2] = ra.gauss(25, .1)
+    return a
 
 if __name__ == '__main__':
-    a = np.asarray([[.5,.5,.5]])
+    a = generate_normal()
+    scalar = MinMaxScaler()
+    scalar.fit(a)
+    a = scalar.transform(a)
     w1 = np.random.rand(2,3)
     w2 = np.random.rand(3,2)
     b1 = .015
     b2 = .015
-    for i in range(100):
-        b, h = feedforward(w1, w2, b1, b2, a)
-        loss = compute_loss(a, b)
-        print(loss)
-        w1, w2, b1, b2 = backpropogate(w1, w2, a, b, h, b1,b2)
+    total_losses = np.zeros((1000,))
+    for j in range(1000):
+        losses = np.zeros((100,))
+        for i in range(100):
+            rand = ra.randint(0, len(a)-1)
+            rand_instance = np.asarray([a[rand]])
+            b, h = feedforward(w1, w2, b1, b2, rand_instance)
+            losses[i] = compute_loss(rand_instance, b)
+            w1, w2, b1, b2 = backpropogate(w1, w2, rand_instance, b, h, b1,b2)
+        total_losses[j] = np.average(losses)
+    plt.plot(total_losses)
+    plt.show()
